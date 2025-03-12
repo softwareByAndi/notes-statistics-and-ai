dbb0b4c44c2a91c4e319b49d97830041d0419c5d
softwarebyandi <softwarebyandi@gmail.com>
2025-03-11 22:00:53 -0600

    leaf-mac 2025-3-11:22:0:52

--- test.py
+++ test.py
@@ -3,10 +3,24 @@ import re
 def adjust_headers_sequential(markdown_text):
     header_pattern = re.compile(r'^(#{1,6})\s+(.*?)$', re.MULTILINE)
     matches = []
+    by_level = {}
     for match in re.finditer(header_pattern, markdown_text):
-        current_header = match.group(1)
+        origin_level = len(match.group(1))
         header_text = match.group(2)
-        
+        current_match = {
+            'origin_level': origin_level,
+            'header_text': header_text
+        }
+        matches.append(current_match)
+        if by_level.get(origin_level) is None:
+            by_level[origin_level] = []
+        by_level[origin_level].append(match)
+    i = 0
+    while i < max_level:
+        i += 1
+        for match in matches:
+
+
 
 # Test with example
 markdown = """## hello
@@ -15,3 +29,21 @@ markdown = """## hello
 ##### dddd"""
 
 print(adjust_headers_sequential(markdown))
+
+
+
+"""
+## a
+#### b
+### c
+###### d
+### e
+"""
+
+"""
+# a
+## b
+## c
+### d
+## e 
+"""
